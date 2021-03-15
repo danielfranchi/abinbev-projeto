04af4ef9ff3199acafb8107dcd93d738512bcf16
danielfranchi <damiel2mf@yahoo.com.br>
2021-03-15 13:53:04 -0300

    beer-emporium-administrator

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,19 +1,18 @@
-import { Route, Switch} from 'react-router-dom'
-import Login from  '../pages/Login/Login'
-import Home from '../pages/Home/Home'
-import Product from '../pages/Product/Product'
-import User from '../pages/User/User'
+import { Route, Switch } from "react-router-dom";
+import Login from "../pages/Login/Login";
+import Home from "../pages/Home/Home";
+import Product from "../pages/Product/Product";
+import User from "../pages/User/User";
 
 function Routes() {
-  return(
+  return (
     <Switch>
-      
       <Route path="/" exact component={Login} />
       <Route path="/home" exact component={Home} />
       <Route path="/product" exact component={Product} />
       <Route path="/user" exact component={User} />
     </Switch>
-  )
+  );
 }
 
-export default Routes
+export default Routes;
